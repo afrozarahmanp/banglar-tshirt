2d21274d88174f9532ea7123daac0bd603415f3e
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-07-12 17:01:08 +0600

    tshirt added but not organized

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from "react-router-dom";
+import Tshirt from "../Tshirt/Tshirt";
 
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
         <div>
-            <h2>This is Home: {tshirts.length}</h2>
+            {
+                tshirts.map(tshirt => <Tshirt
+                    key={tshirt._id}
+                    tshirt={tshirt}
+
+                ></Tshirt>)
+            }
         </div>
     );
 };
---
+++ src/components/Tshirt/Tshirt.jsx
@@ -0,0 +1,12 @@
+
+const Tshirt = ({tshirt}) => {
+    const {picture, name, price} = tshirt;
+    return (
+        <div>
+            <img src={picture} alt=""/>
+            
+        </div>
+    );
+};
+
+export default Tshirt;
