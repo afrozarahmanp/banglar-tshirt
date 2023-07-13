e93a1497a13ec39e0b16de890ebf335b2f92f587
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-07-13 13:20:18 +0600

    Basic structure with tshirts

--- src/components/Tshirt/Tshirt.jsx
+++ src/components/Tshirt/Tshirt.jsx
@@ -1,9 +1,12 @@
-
-const Tshirt = ({tshirt}) => {
+import "./Tshirt.css"
+const Tshirt = ({tshirt, handleAddToCart}) => {
     const {picture, name, price} = tshirt;
     return (
-        <div>
+        <div className="t-shirt">
             <img src={picture} alt=""/>
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <button onClick={() =>handleAddToCart(tshirt)}>Buy Now</button>
             
         </div>
     );
